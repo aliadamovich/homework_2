81e4d361c7b95c873a6d9984a07a74a1b1dd8fd2
aliadamovich <alesya.argentina@gmail.com>
2024-08-03 19:30:46 -0300

    3rd homework

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -35,13 +35,9 @@ const defaultAffairs: Array<AffairType> = [
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
-	
-	if (filter === 'low') return affairs.filter(a => a.priority === 'low')
-	else if (filter === 'middle') return affairs.filter(a => a.priority === 'middle')
-	else if (filter === 'high') return affairs.filter(a => a.priority === 'high')
-	return affairs // need to fix
-	
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { 
+	if (filter === 'all') return affairs
+	return affairs.filter(a => a.priority === filter)
 }
 export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => { // need to fix any
    return affairs.filter(a => a._id !==_id)
